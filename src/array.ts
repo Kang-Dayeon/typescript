808bccdf43578fdba354e2393b4c358223f97014
강다연 <dksldiss89@gmail.com>
2023-06-01 15:00:30 +0900

    array 학습

--- src/array.ts
+++ src/array.ts
@@ -41,6 +41,51 @@ let names = ['Jack', 'Jane', 'Steve']
 
 // ** for...of 문 **
 // 아이템값만 필요한경우
-for(let name of names){
-    console.log(name) // Jack Jane Steve
+// for(let name of names){
+//     console.log(name) // Jack Jane Steve
+// }
+
+// ** 제네릭 타입 **
+// 기본 문법
+// function getText<T>(text: T): T {
+//     return text
+// }
+// getText<string>('hi')
+// getText<number>(10)
+// getText<boolean>(true)
+
+// 함수선언 방법
+// const text1 = getText<string>('Hello World')
+// const text2 = getText('Hello World') // 타입 추론
+
+// T타입의 변수라고 알려줘야 된다
+// 인자로 받은 값의 length를 알고 싶은 경우
+// const arrayLength = <T>(array: T[]): number => array.length // 제네릭 타입이 배열이기 때문에 length를 허용함
+// const isEmpty = <T>(array: T[]): boolean => arrayLength<T>(array) == 0
+
+// let numArray: number[] = [1,2,3]
+// let strArray: string[] = ['hello', 'world']
+//
+// type IPerson = {name: string, age?: number}
+// let personArray: IPerson[] = [{name: 'Jack'}, {name: 'Jane', age: 32}]
+//
+// console.log(
+//     arrayLength(numArray), // 3
+//     arrayLength(strArray), // 2
+//     arrayLength(personArray), // 2
+//     isEmpty([]), // true
+//     isEmpty([1]) // false
+// )
+
+// ** 제네릭 함수의 시그니처 **
+// const normal = (cb: (number) => number): void => {}
+// const error = (cb: (number, number?) => number): void => {} // 식별자 중복 오류
+// const fixed = (cb: (a: number, number?) => number): void => {} // 타입스크립트가 해석하지 못하는 부분에 변수를 삽입하고 타입을 명시하여 해결
+
+// ** range 함수 구현 **
+const range = (from: number, to:number): number[] => {
+    return from < to ? [from, ...range(from + 1, to)] : [] // 재귀 함수 스타일로 동작
 }
+
+let numbers: number[] = range(1, 9 + 1)
+console.log(numbers) // [1,2,3,4,5,6,7,8,9]
