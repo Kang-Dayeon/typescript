1be93da4a49511f97638e9552d340c8027b57773
강다연 <dksldiss89@gmail.com>
2023-11-13 14:47:43 +0900

    Modify 블록체인 만들기

--- src/index.ts
+++ src/index.ts
@@ -18,5 +18,33 @@ class Block implements BlockShape{
     }
     static calculateHash(prevHash:string, height:number, data:string){
         const toHash = `${prevHash}${height}${data}`
+        return crypto.createHash("sha256").update(toHash).digest("hex")
     }
 }
+
+class BlockChain {
+    private blocks: Block[]
+    constructor() {
+        this.blocks = []
+    }
+    public getPrevHash() {
+        if (this.blocks.length === 0) return ""
+        return this.blocks[this.blocks.length - 1].hash
+    }
+    public addBlock(data:string){
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data)
+        this.blocks.push(newBlock)
+    }
+    public getBlock(){
+        // private로 되어있는 원본을 지키기 위해서 복사된 걸 보여주는것
+        return [...this.blocks]
+    }
+}
+
+const blockchain = new BlockChain()
+
+blockchain.addBlock("First one")
+blockchain.addBlock("Second one")
+blockchain.addBlock("Third one")
+
+console.log(blockchain.getBlock())
